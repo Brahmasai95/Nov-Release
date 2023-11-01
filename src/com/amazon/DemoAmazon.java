5b95669ff1c6afb9054cfc5c1bb2ee53e7de8f1d
brahmasai <brahmasai96@gmail.com>
2023-11-01 20:38:11 +0530

    new changes

--- src/com/amazon/DemoAmazon.java
+++ src/com/amazon/DemoAmazon.java
@@ -3,7 +3,7 @@ package com.amazon;
 public class DemoAmazon {
 	public void hello() {
 		System.out.println("brahmasai welcome to GIT");
-		System.out.println("brahmasai welcome to java");
+		System.out.println("brahmasai welcome to javaaa");
 
 	}
 
