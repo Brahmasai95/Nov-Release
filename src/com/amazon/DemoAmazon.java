eb688602cf01bbd8415586de46e40d091e218115
brahmasai <brahmasai96@gmail.com>
2023-11-01 20:03:19 +0530

    i changed my cls

--- src/com/amazon/DemoAmazon.java
+++ src/com/amazon/DemoAmazon.java
@@ -1,5 +1,10 @@
 package com.amazon;
 
 public class DemoAmazon {
+	public void hello() {
+		System.out.println("brahmasai welcome to GIT");
+		System.out.println("brahmasai welcome to java");
+
+	}
 
 }
