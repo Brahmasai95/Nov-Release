c3c844f494e8a32c2bdf12cb3c6f689c7d102a02
brahmasai <brahmasai96@gmail.com>
2023-11-03 20:24:18 +0530

    changing the code

--- src/com/amazon/DemoAmazon.java
+++ src/com/amazon/DemoAmazon.java
@@ -2,9 +2,13 @@ package com.amazon;
 
 public class DemoAmazon {
 	public void hello() {
-		System.out.println("brahmasai welcome to GIT");
-		System.out.println("brahmasai welcome to qwjkd");
+		System.out.println("this hgasdba");
+		System.out.println("wkgdwqiugdqw");
 
 	}
+	public static void main(String[] args) {
+		System.out.println("akjfgajfge");
+		System.out.println("ekufhiu");
 
+	}
 }
